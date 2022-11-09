c54c777269af42d435158b7f8b1062e7a6860778
mohammed <m1aladham@gmail.com>
2022-11-09 10:40:54 +0000

    Sync LeetCode submission - Number of Days Between Two Dates (php)

---
+++ my-folder/problems/number_of_days_between_two_dates/solution.php
@@ -0,0 +1,17 @@
+class Solution {
+
+    /**
+     * @param String $date1
+     * @param String $date2
+     * @return Integer
+     */
+    function daysBetweenDates($date1, $date2) {
+        
+        $date1 = strtotime($date1);
+        $date2 = strtotime($date2);
+        $datediff = $date1 - $date2;
+
+        return abs(round($datediff / (60 * 60 * 24)));
+
+    }
+}
