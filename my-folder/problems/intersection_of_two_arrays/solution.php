f5cf4dd2736fadaaad9ac3c20b0dc1bf1d68bd1d
mohammed <m1aladham@gmail.com>
2022-11-09 09:25:36 +0000

    Sync LeetCode submission - Intersection of Two Arrays (php)

---
+++ my-folder/problems/intersection_of_two_arrays/solution.php
@@ -0,0 +1,26 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Integer[]
+     */
+    function intersection($nums1, $nums2) {
+        $hash = [];
+        $arr = [];
+
+        foreach($nums1 as $n){
+            $hash[$n] = 1;
+        }
+        
+        foreach($nums2 as $n){
+            if(isset($hash[$n]) && $hash[$n] === 1){
+                $hash[$n] = ++$hash[$n];
+                $arr[] = $n;
+            }
+        }
+
+
+        return $arr;
+    }
+}
