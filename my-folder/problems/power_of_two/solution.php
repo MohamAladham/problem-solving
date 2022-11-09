25cd4b949cb221ba4c1d2d424349d0270e9d057c
mohammed <m1aladham@gmail.com>
2022-11-09 11:32:22 +0000

    Sync LeetCode submission - Power of Two (php)

---
+++ my-folder/problems/power_of_two/solution.php
@@ -0,0 +1,25 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isPowerOfTwo($n) {
+       return $this->sol1($n);
+    }
+
+
+    function sol1($n){
+        $sum = 1;
+
+        while(true){
+            if($sum>$n){
+                return false;
+            }elseif($sum==$n){
+                return true;
+            }
+
+            $sum = $sum * 2;
+        }
+    }
+}
