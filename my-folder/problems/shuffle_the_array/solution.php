315a97f9c99b01565e14b41b5b28def459db898e
mohammed <m1aladham@gmail.com>
2022-11-10 07:11:10 +0000

    Sync LeetCode submission - Shuffle the Array (php)

---
+++ my-folder/problems/shuffle_the_array/solution.php
@@ -0,0 +1,38 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $n
+     * @return Integer[]
+     */
+    function shuffle($nums, $n) {
+        $new_arr = [];
+        $left = 0;
+        $right = $n;
+      
+        
+        if($right === $left){
+            return $nums;
+        }
+
+
+        while(true){
+         
+            if(isset($nums[$left])){
+                $new_arr[] = $nums[$left];
+                $left++;
+            }
+            
+            if(isset($nums[$right])){
+                $new_arr[] = $nums[$right];
+                $right++;
+            }
+         
+            if($left === $n){
+                break;
+            }
+        }
+
+        return $new_arr;
+    }
+}
