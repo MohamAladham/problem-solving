b26acecf24d968b2515272763d23f4afa55c96e8
mohammed <m1aladham@gmail.com>
2022-11-10 08:52:23 +0000

    Sync LeetCode submission - Partitioning Into Minimum Number Of Deci-Binary Numbers (php)

---
+++ my-folder/problems/partitioning_into_minimum_number_of_deci-binary_numbers/solution.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param String $n
+     * @return Integer
+     */
+    function minPartitions($n) {
+       
+       	return max(str_split($n));
+
+        $n = str_split($n);
+        $max = 0;
+
+        foreach($n as $a){
+            $max = max($max, $a);
+        }
+
+        return $max;
+    }
+}
