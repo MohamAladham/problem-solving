f52359dbb204235a6a5b7417916005ebfe163880
mohammed <m1aladham@gmail.com>
2022-11-17 14:42:36 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (php)

---
+++ my-folder/problems/minimum_size_subarray_sum/solution.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param Integer $target
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function minSubArrayLen($target, $nums) {
+    
+        $count  = count( $nums );
+        $result = PHP_INT_MAX;
+        $sum    = 0;
+        $left   = 0;
+
+        for ( $r = $left; $r < $count; $r++ )
+        {
+            $sum += $nums[$r];
+            while ( $sum >= $target )
+            {
+                $result = min( $r - $left + 1, $result );
+                $sum    -= $nums[$left];
+                $left++;
+            }
+        }
+
+        return $result == PHP_INT_MAX ? 0 : $result;   
+    }
+}
