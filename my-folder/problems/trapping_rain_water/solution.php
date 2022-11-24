86da18b6cd351713faf3d0a380c20024e2d6a17c
mohammed <m1aladham@gmail.com>
2022-11-24 23:53:50 +0000

    Sync LeetCode submission - Trapping Rain Water (php)

---
+++ my-folder/problems/trapping_rain_water/solution.php
@@ -0,0 +1,33 @@
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function trap($height) {
+        $water = 0;
+        $count = count( $height );
+        $stack = new SplStack();
+
+        for ( $i = 0; $i < $count; $i++ )
+        {
+            while ( !$stack->isEmpty() && $height[$i] > $height[$stack->top()] )
+            {
+                $top = $stack->pop();
+
+                if ( $stack->isEmpty() )
+                {
+                    break;
+                }
+
+                $d     = $i - $stack->top() - 1;
+                $water += $d * 
+                    ( min( $height[$i], $height[$stack->top()] ) - $height[$top] );
+            }
+
+            $stack->push( $i );
+        }
+
+        return $water;
+    }
+}
