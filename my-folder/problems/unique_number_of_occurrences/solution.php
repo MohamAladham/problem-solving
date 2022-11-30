7986488a7e85718d5f555fe2ccbdc2f6aa91aa2f
mohammed <m1aladham@gmail.com>
2022-11-30 13:25:58 +0000

    Sync LeetCode submission - Unique Number of Occurrences (php)

---
+++ my-folder/problems/unique_number_of_occurrences/solution.php
@@ -0,0 +1,25 @@
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function uniqueOccurrences($arr) {
+        $hash_1 = [];
+        $hash_2 = [];
+
+        foreach($arr as $a){
+            $hash[$a] = isset($hash[$a]) ? $hash[$a]+1 : 1;
+        }
+            
+        foreach($hash as $k=>$v){
+            if(isset($hash_2[$v])){
+                return false;
+            }
+
+            $hash_2[$v] = true;
+        }
+
+        return true;
+    }
+}
