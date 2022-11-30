3cb5e0af16900575977451e9a3ef9693f066ede3
mohammed <m1aladham@gmail.com>
2022-11-30 17:11:23 +0000

    Sync LeetCode submission - Contains Duplicate (php)

---
+++ my-folder/problems/contains_duplicate/solution.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function containsDuplicate($nums) {
+        $hash = [];
+
+        foreach($nums as $n){
+            if(isset($hash[$n])){
+                return true;
+            }
+
+            $hash[$n] = true;
+        }
+
+        return false;
+    }
+}
