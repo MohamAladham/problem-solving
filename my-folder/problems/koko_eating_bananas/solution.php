4a921f4354819edb619bf9261da7a0f5d0d3d54c
mohammed <m1aladham@gmail.com>
2022-12-06 21:00:37 +0000

    Sync LeetCode submission - Koko Eating Bananas (php)

---
+++ my-folder/problems/koko_eating_bananas/solution.php
@@ -0,0 +1,32 @@
+class Solution {
+
+    /**
+     * @param Integer[] $piles
+     * @param Integer $h
+     * @return Integer
+     */
+    function minEatingSpeed($piles, $h) {
+        $max = max($piles);
+        $l = 1;
+        $r = $max;
+        $res = $max;
+
+        while($l<=$r){
+            $mid = (int)(($l+$r)/2);
+            $time = 0;
+
+            foreach($piles as $p){
+                $time += ceil($p/$mid);
+            }
+
+            if($time > $h){
+                $l = $mid + 1;
+            }else{
+                $res = min($mid, $res);
+                $r = $mid - 1;
+            }
+        }
+
+        return $res;
+    }
+}
