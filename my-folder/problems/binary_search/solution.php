01cc619e47fe94c0bb549bb807b7c2ef99beec9b
mohammed <m1aladham@gmail.com>
2022-12-06 10:07:26 +0000

    Sync LeetCode submission - Binary Search (php)

---
+++ my-folder/problems/binary_search/solution.php
@@ -0,0 +1,27 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function search($nums, $target) {
+       $left = 0;
+       $right = count($nums) - 1;
+
+       while($left<=$right){
+           $mid = (int) (($right + $left) / 2);
+           $found = $nums[$mid];
+
+           if($found === $target){
+               return $mid;
+           }elseif($found>$target){
+               $right = $mid - 1;
+           }else{
+               $left = $mid + 1;
+           }
+       }
+
+        return -1;
+    }
+}
