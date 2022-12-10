a5f0ebdc828ae228c9492946eff537f56ec57b34
mohammed <m1aladham@gmail.com>
2022-12-10 22:56:22 +0000

    Sync LeetCode submission - Minimum Number of Arrows to Burst Balloons (php)

---
+++ my-folder/problems/minimum_number_of_arrows_to_burst_balloons/solution.php
@@ -0,0 +1,23 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $points
+     * @return Integer
+     */
+    function findMinArrowShots($points) {
+        sort($points);
+        $end=$points[0][1];
+	    $res=1;
+
+    	for ($i=1; $i<count($points); $i++) {
+			if ($points[$i][0]<=$end) {
+				$end = min($end,$points[$i][1]);
+			} else {
+				$end=$points[$i][1];
+				$res++;
+			}
+		}
+
+        return $res;
+    }
+}
