241b2fd4142ea99324f346dc9eaf0c69cba01e27
mohammed <m1aladham@gmail.com>
2022-12-22 23:50:12 +0000

    Sync LeetCode submission - Subsets (php)

---
+++ my-folder/problems/subsets/solution.php
@@ -0,0 +1,29 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    function subsets($nums) {
+        $list = [];
+        $tempList = [];
+        $this->backtrack( $list, $tempList, $nums, 0 );
+
+        return $list;
+    }
+
+
+
+    function backtrack( &$list, $tempList, $nums, $start )
+    {
+        $list[] = $tempList;
+
+        for ( $i = $start; $i < count( $nums ); $i++ )
+        {
+            $tempList[] = $nums[$i];
+            $this->backtrack( $list, $tempList, $nums, $i + 1 );
+            array_pop( $tempList );
+        }
+    }
+
+}
