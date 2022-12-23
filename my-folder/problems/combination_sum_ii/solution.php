5148c7cddd1d73e4cd719764cfa2209547bad92f
mohammed <m1aladham@gmail.com>
2022-12-23 18:30:41 +0000

    Sync LeetCode submission - Combination Sum II (php)

---
+++ my-folder/problems/combination_sum_ii/solution.php
@@ -0,0 +1,58 @@
+
+class Solution
+{
+
+    private $candidates;
+    private $target;
+
+
+    /**
+     * @param Integer[] $candidates
+     * @param Integer $target
+     * @return Integer[][]
+     */
+    function combinationSum2( $candidates, $target )
+    {
+        $list             = [];
+        sort($candidates);
+        $this->candidates = $candidates;
+        $this->target     = $target;
+        $this->backtrack( $list, [], 0 );
+
+        return $list;
+    }
+
+
+    function backtrack( &$list, $try, $start )
+    {
+        $sum = 0;
+        foreach ( $try as $t )
+        {
+            $sum += $t;
+        }
+
+        if ( $sum === $this->target )
+        {
+            $list[] = $try;
+
+            return;
+        }
+
+        if ( $sum > $this->target )
+        {
+            return;
+        }
+
+        for ( $i = $start; $i < count( $this->candidates ); $i++ )
+        {
+            if ( $i > $start && $this->candidates[$i] == $this->candidates[$i-1] )
+            {
+                continue;
+            }
+
+            $try[] = $this->candidates[$i];
+            $this->backtrack( $list, $try, $i + 1 );
+            array_pop( $try );
+        }
+    }
+}
