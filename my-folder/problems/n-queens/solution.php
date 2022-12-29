275ab090e07665fbaabfc12cd6a5ced1c38ac411
mohammed <m1aladham@gmail.com>
2022-12-29 12:31:28 +0000

    Sync LeetCode submission - N-Queens (php)

---
+++ my-folder/problems/n-queens/solution.php
@@ -0,0 +1,58 @@
+class Solution {
+
+    private $sol = [];
+
+    /**
+     * @param Integer $n
+     * @return String[][]
+     */
+    function solveNQueens($n) {
+        // $board = ['....', '....', '....', '....']
+        $board = array_fill(0, $n, str_pad('', $n, '.'));
+        $this->backtracking($board, 0);
+        return $this->sol;
+    }
+
+
+    function backtracking(&$board, $row){
+        // is this the last row? we found a solution.
+        if($row === count($board)){
+            $this->sol[] = $board;
+            return;
+        }
+
+        // check each column for the $row
+        for($col=0; $col<count($board); $col++){
+            if($this->isSafe($board, $row, $col)){
+                $board[$row][$col] = 'Q';
+                // check next row.
+                $this->backtracking($board, $row+1);
+                // backtrack to get other possible solutions.
+                $board[$row][$col] = '.';
+            }
+        }
+    }
+
+    function isSafe(&$board, $row, $col){
+        $n = count($board);
+
+        for($i=0; $i<$n; $i++){
+            // is there a queen in the same column?
+            if($board[$i][$col] === 'Q'){
+                return false;
+            }
+
+            // is there a queen in the same positive diagonal?
+            if($row-$i >= 0 && $col-$i >= 0 && $board[$row-$i][$col-$i] === 'Q'){
+return false;
+            }
+
+            // is there a queen in the same negative diagonal?   
+            if($row-1 >= 0 && $col+$i < $n && $board[$row-$i][$col+$i] === 'Q'){
+return false;
+            }
+        }
+
+        return true;
+    }
+}
