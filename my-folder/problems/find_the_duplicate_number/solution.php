30c91f6c00a331bdb2e5240eb34b95d1508b21f6
mohammed <m1aladham@gmail.com>
2022-12-29 13:26:17 +0000

    Sync LeetCode submission - Find the Duplicate Number (php)

---
+++ my-folder/problems/find_the_duplicate_number/solution.php
@@ -0,0 +1,16 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findDuplicate($nums) {
+        sort($nums);
+
+        for($i=1; $i<count($nums); $i++){
+            if($nums[$i] === $nums[$i-1]){
+                return $nums[$i];
+            }    
+        }
+    }
+}
