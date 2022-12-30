9f2bb5a0702fbc5af25692528c759f89552bdfef
mohammed <m1aladham@gmail.com>
2022-12-30 16:35:00 +0000

    Sync LeetCode submission - Reverse Words in a String III (php)

---
+++ my-folder/problems/reverse_words_in_a_string_iii/solution.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function reverseWords($s) {
+        $ans = '';
+        $stack = new splStack();
+
+        for($r=0; $r<=strlen($s); $r++){
+             if($s[$r] === ' ' || $r === strlen($s)){
+                while(!$stack->isEmpty()){
+                    $ans .= $stack->pop();
+                }    
+
+                if($r !== strlen($s)){
+                    $ans .= ' ';
+                }
+             }else{
+                 $stack->push($s[$r]);
+             }
+        }
+
+
+        return $ans;
+    }
+}
