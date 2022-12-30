07e57f258f14dc4eba6ab49e2261692abc7bc2d4
mohammed <m1aladham@gmail.com>
2022-12-30 15:37:27 +0000

    Sync LeetCode submission - Find Greatest Common Divisor of Array (php)

---
+++ my-folder/problems/find_greatest_common_divisor_of_array/solution.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findGCD($nums) {
+        $max = max($nums);
+        $min = min($nums);
+
+        for($i=$min; $i>0; $i--){
+            if($max%$i === 0 && $min%$i === 0){
+                return $i;
+            }
+        }
+
+        return 1;
+    }
+}
