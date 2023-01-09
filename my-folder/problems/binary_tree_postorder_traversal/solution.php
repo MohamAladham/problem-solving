4b7628f867e5ce1083c6bebd048166d07d249fb6
mohammed <m1aladham@gmail.com>
2023-01-09 08:48:45 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (php)

---
+++ my-folder/problems/binary_tree_postorder_traversal/solution.php
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    function postorderTraversal($root) {
+        $res = [];
+        $this->DFS($root, $res);
+        return $res;
+    }
+
+
+    function DFS($root, &$res){
+        if(!$root){
+            return;
+        }
+
+        $this->DFS($root->left, $res);
+        $this->DFS($root->right, $res);
+        $res[] = $root->val;
+    }
+    
+}
