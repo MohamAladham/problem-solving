57563f1f8ca1ecc1ee156ca660137ac7aa720848
mohammed <m1aladham@gmail.com>
2023-01-13 13:27:32 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (php)

---
+++ my-folder/problems/kth_smallest_element_in_a_bst/solution.php
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $k
+     * @return Integer
+     */
+    function kthSmallest($root, $k) {
+        $arr = [];
+        $this->DFS($root, $k, $arr);
+        return $arr[$k - 1];
+    }
+
+
+    function DFS($root, $k, &$arr){
+        if(!$root){
+            return;
+        }
+
+        $this->DFS($root->left, $k, $arr);
+        $arr[] = $root->val;
+        $this->DFS($root->right, $k, $arr);
+    }
+}
