8623b1e3b0f2629b5dede4a5e68e89d78c86b688
mohammed <m1aladham@gmail.com>
2023-01-16 13:41:49 +0000

    Sync LeetCode submission - Flood Fill (php)

---
+++ my-folder/problems/flood_fill/solution.php
@@ -0,0 +1,33 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $image
+     * @param Integer $sr
+     * @param Integer $sc
+     * @param Integer $color
+     * @return Integer[][]
+     */
+    function floodFill($image, $sr, $sc, $color) {
+        $this->sameColor = $image[$sr][$sc];
+        
+        if($this->sameColor !== $color){
+            $this->DFS($image, $sr, $sc, $color);
+        }
+
+        return $image;
+    }
+
+
+    function DFS(&$image, $sr, $sc, $color){
+        if(!isset($image[$sr][$sc]) || $image[$sr][$sc] !== $this->sameColor){
+            return;
+        }
+
+        $image[$sr][$sc] = $color;
+
+        $this->DFS($image, $sr-1, $sc, $color);
+        $this->DFS($image, $sr+1, $sc, $color);
+        $this->DFS($image, $sr, $sc-1, $color);
+        $this->DFS($image, $sr, $sc+1, $color);
+    }
+}
