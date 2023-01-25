b201d6ba7445429d96ca9d67a585d83bfeecb0fc
mohammed <m1aladham@gmail.com>
2023-01-25 19:51:36 +0000

    Sync LeetCode submission - Maximum Product Subarray (php)

---
+++ my-folder/problems/maximum_product_subarray/solution.php
@@ -0,0 +1,28 @@
+class Solution {
+
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function maxProduct($nums) {
+        $ans = $max = $min = $nums[0];
+     
+        for($i=1; $i<count($nums); $i++){
+            $n = $nums[$i];
+           
+            if($n < 0){
+                $temp = $max;
+                $max = $min;
+                $min = $temp;
+            }
+   
+            $max = max($n, $max*$n);
+            $min = min($n, $min*$n);
+         
+            $ans = max($max, $ans);
+        }
+
+        return $ans;
+    }
+}
