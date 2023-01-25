38c19624a693d028c351d0152a0c5c212a2f923c
mohammed <m1aladham@gmail.com>
2023-01-25 17:11:49 +0000

    Sync LeetCode submission - Divide Two Integers (php)

---
+++ my-folder/problems/divide_two_integers/solution.php
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param Integer $dividend
+     * @param Integer $divisor
+     * @return Integer
+     */
+    function divide($dividend, $divisor) {
+        $positive = ($dividend < 0) == ($divisor < 0);
+        $dividend = abs($dividend);
+        $divisor = abs($divisor);
+        $res = 0;
+        
+        while ($dividend >= $divisor) {
+            $temp = $divisor;
+            $i = 1;
+            while ($dividend >= $temp) {
+                $dividend -= $temp;
+                $res += $i;
+                $i += $i;
+                $temp += $temp;
+            }
+        }
+
+        if (!$positive) {
+            $res = -$res;
+        }
+        
+        return min(max(-2147483648, $res), 2147483647);
+    }
+}
