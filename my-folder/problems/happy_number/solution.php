3b572b1866cb4574a2357b35c065730883825374
mohammed <m1aladham@gmail.com>
2023-02-12 20:27:02 +0000

    Sync LeetCode submission - Happy Number (php)

---
+++ my-folder/problems/happy_number/solution.php
@@ -0,0 +1,35 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isHappy($n) {
+        $sumFast = $sumLow = $n;
+
+        while(true){
+            $sumLow = $this->sumPow($sumLow);
+            $sumFast = $this->sumPow($this->sumPow($sumFast));
+            
+            if($sumFast === 1){
+                return true;
+            }
+            
+            if($sumFast === $sumLow){
+                return false;
+            }
+        }
+    }
+
+
+    function sumPow($n){
+        $n = str_split($n);
+        $sum = 0;
+
+        foreach($n as $d){
+            $sum += pow($d, 2);
+        }    
+
+        return $sum;
+    }
+}
