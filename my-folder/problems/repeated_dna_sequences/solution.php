9899031ca82d00e306869a0c628c8bf33cc0b974
mohammed <m1aladham@gmail.com>
2023-02-23 12:49:27 +0000

    Sync LeetCode submission - Repeated DNA Sequences (php)

---
+++ my-folder/problems/repeated_dna_sequences/solution.php
@@ -0,0 +1,30 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String[]
+     */
+    function findRepeatedDnaSequences($s) {
+        $k = 10;
+        $s_len = strlen($s);
+        
+        if($s_len <= $k){
+            return [];
+        }
+        
+        $hash = [];
+        $output = [];
+
+        for($left=0; $left<$s_len; $left++){
+            $sequence = substr($s, $left, $k);
+
+            if(isset($hash[$sequence]) && $hash[$sequence] === 1){
+                $output[] = $sequence;
+            }
+
+            $hash[$sequence] = isset($hash[$sequence]) ? $hash[$sequence]+1 : 1;
+        }
+
+        return $output;
+    }
+}
