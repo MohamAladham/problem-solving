b156bec0250f84375b3454068d34a64f09dd1b74
mohammed <m1aladham@gmail.com>
2023-02-25 20:43:15 +0000

    Sync LeetCode submission - Reverse Linked List II (php)

---
+++ my-folder/problems/reverse_linked_list_ii/solution.php
@@ -0,0 +1,44 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @param Integer $left
+     * @param Integer $right
+     * @return ListNode
+     */
+    function reverseBetween($head, $left, $right) {
+        $dummy = new ListNode();
+        $dummy->next = $head;
+        $pre = $dummy;
+        
+        for($i=1; $i<=$left-1; $i++){
+            $pre = $pre->next;
+        }
+
+        $start = $pre->next;
+        $next = $start->next;
+    
+        // 1 - 2 - 3 - 4 - 5 ; left=2; right =4 ---> pre = 1, start = 2, next = 3
+        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+
+        for($i=0; $i<$right-$left; $i++){
+            $start->next = $next->next;
+            $next->next = $pre->next;
+            $pre->next = $next;
+            $next = $start->next;    
+        }
+
+        return $dummy->next;
+    }
+}
