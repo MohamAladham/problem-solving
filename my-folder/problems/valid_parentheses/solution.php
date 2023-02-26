137d03329327426908aedeb581ecd2dd96788e4a
mohammed <m1aladham@gmail.com>
2023-02-26 22:23:48 +0000

    Sync LeetCode submission - Valid Parentheses (php)

--- my-folder/problems/valid_parentheses/solution.php
+++ my-folder/problems/valid_parentheses/solution.php
@@ -5,38 +5,26 @@ class Solution {
      * @return Boolean
      */
     function isValid($s) {
-        $s = str_split($s);
-        $stack= [];
-        
-        if(count($s) % 2 !==0){
+        if(strlen($s) % 2 !==0){
             return false;
         }
         
-        // [)
+        $stack = new SplStack();
         
-        foreach($s as $p){
-            $peek = $stack[array_key_last($stack)];
-            
-            if(in_array($p, ['(','[','{'])){
-                $stack[] = $p;
-            }
-            elseif($stack && $p === ')' && $peek === '('){
-                array_pop($stack);
-            }
-            
-            elseif($stack && $p === ']' && $peek === '['){
-                array_pop($stack);
-            }
-            
-            elseif($stack && $p === '}' && $peek === '{'){
-                array_pop($stack);
-            }else{
+        for($i=0; $i<strlen($s); $i++){
+            $chr = $s[$i];
+
+            if($chr === '(' ){
+                $stack->push(')');
+            }elseif($chr === '[' ){
+                $stack->push(']');
+            }elseif($chr === '{' ){
+                $stack->push('}');
+            }elseif($stack->isEmpty() || $stack->pop() !== $chr){
                 return false;
             }
-            
         }
-        
-        var_dump($stack);
-        return !$stack;
+
+        return $stack->isEmpty();
     }
 }
