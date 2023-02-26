5bc6a889a5e35d93d7e88bb2feb3abd12da93a9c
mohammed <m1aladham@gmail.com>
2023-02-26 20:08:17 +0000

    Sync LeetCode submission - Daily Temperatures (php)

--- my-folder/problems/daily_temperatures/solution.php
+++ my-folder/problems/daily_temperatures/solution.php
@@ -5,19 +5,34 @@ class Solution {
      * @return Integer[]
      */
     function dailyTemperatures($temperatures) {
-        $stack = [];
-        $result = array_fill(0, count($temperatures), 0);
+/*
+[73,74,75,71,69,72,76,73]
+ 0.  1. 2. 3. 4. 5. 6. 7
+[1,  1, 4, 2, 1, 1, 0, 0]
+*/
 
-        foreach($temperatures as $index=>$t){
-            while($stack && $t > $temperatures[end($stack)]){
-                $stacked_index = array_pop($stack);
-                $result[$stacked_index] = $index - $stacked_index;
+/*
+
+
+4:69
+3:71
+*/
+
+        $stack = new SplStack();
+        $ans = array_fill(0,count($temperatures), 0);
+
+        for($i=0; $i<count($temperatures); $i++){
+            while(!$stack->isEmpty() && $temperatures[$i]>$temperatures[$stack->top()]){
+                $temp = $temperatures[$stack->top()];
+                
+                if($temperatures[$i] > $temp){
+                    $ans[$stack->top()] = $i - $stack->pop();
+                }
             }
 
-            array_push($stack, $index);
+            $stack->push($i);
         }
 
-
-        return $result;
+        return $ans;
     }
 }
