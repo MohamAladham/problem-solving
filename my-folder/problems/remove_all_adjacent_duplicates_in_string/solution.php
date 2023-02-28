89c421f26de78f4b6d5e59f32c37f3406e948883
mohammed <m1aladham@gmail.com>
2023-02-28 12:30:31 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates In String (php)

---
+++ my-folder/problems/remove_all_adjacent_duplicates_in_string/solution.php
@@ -0,0 +1,25 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function removeDuplicates($s) {
+        $stack = new SplStack();
+        $ans = '';
+
+        for($i=0; $i<strlen($s); $i++){
+            if(!$stack->isEmpty() && $stack->top() === $s[$i]){
+                $stack->pop();
+            }else{
+                $stack->push($s[$i]);
+            }
+        }
+
+        while(!$stack->isEmpty()){
+            $ans .= $stack->shift();
+        }
+
+        return $ans;
+    }
+}
