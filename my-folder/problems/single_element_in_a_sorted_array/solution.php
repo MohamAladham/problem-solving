cc287a7e9eda7fd4ab8a6bf710c010289c08d0c4
mohammed <m1aladham@gmail.com>
2023-03-04 22:07:10 +0000

    Sync LeetCode submission - Single Element in a Sorted Array (php)

---
+++ my-folder/problems/single_element_in_a_sorted_array/solution.php
@@ -0,0 +1,43 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function singleNonDuplicate($nums) {
+        $l = 0;
+        $r = count($nums) - 1;
+
+        // [1,1,2,3,3,4,4,8,8]
+        //  0,1,2,3,4,5,6,7,8
+
+        while ($l <= $r) {
+            $m = floor(($l + $r) / 2);
+
+            // in the middle of the array
+            if((isset($nums[$m-1]) && isset($nums[$m+1]) && $nums[$m-1] != $nums[$m] && $nums[$m+1] != $nums[$m]))
+            {
+                return $nums[$m];
+            }
+
+            // last element
+            if(!isset($nums[$m+1]) && $nums[$m] != $nums[$m-1]){
+                return $nums[$m];
+            }
+
+            // first element
+            if(!isset($nums[$m-1]) && $nums[$m] != $nums[$m+1]){
+                return $nums[$m];
+            }
+
+            $leftSize = isset($nums[$m-1]) && $nums[$m-1] == $nums[$m] ? $m - 1 : $m;
+
+            // is odd? has unque number.
+            if($leftSize%2 != 0){
+                $r = $m - 1;
+            }else{
+                $l = $m + 1;
+            }
+        }
+    }
+}
