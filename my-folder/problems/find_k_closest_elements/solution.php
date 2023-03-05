1f0041b2546377dca4b89b7bdfd788d06df8d26d
mohammed <m1aladham@gmail.com>
2023-03-05 17:08:47 +0000

    Sync LeetCode submission - Find K Closest Elements (php)

---
+++ my-folder/problems/find_k_closest_elements/solution.php
@@ -0,0 +1,29 @@
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @param Integer $k
+     * @param Integer $x
+     * @return Integer[]
+     */
+    function findClosestElements($arr, $k, $x) {
+        $left = 0;
+        $right = count($arr) - 1;
+
+        while ($right - $left >= $k) {
+            if (abs($arr[$right] - $x) < abs($arr[$left] - $x)) {
+                $left++;
+            } else {
+                $right--;
+            }   
+        }
+
+        $res = [];
+        for ($i = $left; $i <= $right; $i++) {
+            $res[] = $arr[$i];
+        }
+ 
+        return $res;
+    }
+
+}
