9f65c1faa9e3603b3cc47103673db191f94a7341
mohammed <m1aladham@gmail.com>
2023-03-05 14:58:03 +0000

    Sync LeetCode submission - Intersection of Multiple Arrays (php)

---
+++ my-folder/problems/intersection_of_multiple_arrays/solution.php
@@ -0,0 +1,27 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $nums
+     * @return Integer[]
+     */
+    function intersection($nums) {
+        $hash = [];
+        $ans = [];
+        
+        foreach($nums as $n){
+            foreach($n as $nn){
+                $hash[$nn] = isset($hash[$nn]) ? $hash[$nn] + 1 : 1;
+            }
+        }
+        
+        foreach($hash as $k=>$v){
+            if($v === count($nums)){
+                $ans[] = $k;
+            }
+        }
+        
+        sort($ans);
+        
+        return $ans;
+    }
+}
