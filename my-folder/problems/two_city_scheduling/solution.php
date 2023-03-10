1fd9d9986b94367a961678b01b9b4344df27166e
mohammed <m1aladham@gmail.com>
2023-03-10 21:15:04 +0000

    Sync LeetCode submission - Two City Scheduling (php)

---
+++ my-folder/problems/two_city_scheduling/solution.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $costs
+     * @return Integer
+     */
+    function twoCitySchedCost($costs) {
+        $total = 0;
+        $differences = [];
+        $people = count($costs);
+
+        foreach($costs as $c){
+            $differences[] = [$c[0]-$c[1], $c[0], $c[1]];
+        }
+
+        sort($differences);
+
+        foreach($differences as $i=>$diff){
+            if($people/2 > $i){
+                $total += $diff[1];
+            }else{
+                $total += $diff[2];
+            }
+        }
+
+        return $total;
+    }
+}
