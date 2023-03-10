3d690e35a2346228a8e86ad4cdd22faddf75b22b
mohammed <m1aladham@gmail.com>
2023-03-10 20:19:55 +0000

    Sync LeetCode submission - Gas Station (php)

---
+++ my-folder/problems/gas_station/solution.php
@@ -0,0 +1,27 @@
+class Solution {
+
+    /**
+     * @param Integer[] $gas
+     * @param Integer[] $cost
+     * @return Integer
+     */
+    function canCompleteCircuit($gas, $cost) {
+        $startingIndex = 0;
+        $totalGas = 0;
+
+        if(array_sum($cost) > array_sum($gas)){
+            return -1;
+        }
+
+        for($i=0; $i<count($gas); $i++){
+            $totalGas += $gas[$i] - $cost[$i];
+
+            if($totalGas < 0){
+                $totalGas = 0;
+                $startingIndex = $i+1;
+            }
+        }
+
+        return $startingIndex;
+    }
+}
