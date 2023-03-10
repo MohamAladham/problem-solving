0261b175da20b0dcc007ebf3cbe2dd02977d924f
mohammed <m1aladham@gmail.com>
2023-03-10 16:35:11 +0000

    Sync LeetCode submission - Generate Parentheses (php)

---
+++ my-folder/problems/generate_parentheses/solution.php
@@ -0,0 +1,35 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return String[]
+     */
+    function generateParenthesis($n) {
+        $result = [];
+
+        $this->backtrack(n: $n, openCount: 0, closeCount: 0, path: [], result: $result);
+
+        return $result;
+    }
+
+
+    function backtrack(int $n, int $openCount, int $closeCount, array $path, array &$result):void
+    {
+        if($openCount === $closeCount && $openCount === $n){
+            $result[] = implode('', $path);
+            return;
+        }
+
+        if($openCount < $n){
+            $path[] = '(';
+            $this->backtrack($n, $openCount+1, $closeCount, $path, $result);
+            array_pop($path);
+        }
+
+        if($closeCount < $openCount){
+            $path[] = ')';
+            $this->backtrack($n, $openCount, $closeCount+1, $path, $result);
+            array_pop($path);
+        }
+    }
+}
