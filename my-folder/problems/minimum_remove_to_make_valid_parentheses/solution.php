149f43e3b414e59ca30d3890364bd16c16b27978
mohammed <m1aladham@gmail.com>
2023-03-11 12:41:51 +0000

    Sync LeetCode submission - Minimum Remove to Make Valid Parentheses (php)

---
+++ my-folder/problems/minimum_remove_to_make_valid_parentheses/solution.php
@@ -0,0 +1,26 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function minRemoveToMakeValid($s) {
+        $stack = new SplStack();
+        $ans = $s;
+
+        for($i=0; $i<strlen($s); $i++){
+            if($s[$i] === ')' && !$stack->isEmpty() && $stack->top()[0] === '('){
+                $stack->pop();
+            }elseif($s[$i] === ')' || $s[$i] === '('){
+                $stack->push([$s[$i], $i]);
+            }
+        }
+
+        while(!$stack->isEmpty()){
+            $index = $stack->pop()[1];
+            $ans = substr($ans, 0, $index) . substr($ans, $index + 1, strlen($ans));
+        }
+
+        return $ans;
+    }
+}
