1193965ab626e707d86151a0a8fdcc268051d76e
mohammed <m1aladham@gmail.com>
2023-03-13 13:16:35 +0000

    Sync LeetCode submission - Sort Colors (php)

---
+++ my-folder/problems/sort_colors/solution.php
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return NULL
+     */
+    function sortColors(&$nums) {
+        $colorsHash = [];
+
+        for($i=0; $i<count($nums); $i++){
+            $colorsHash[$nums[$i]] = isset($colorsHash[$nums[$i]]) ? $colorsHash[$nums[$i]]+1: 1; 
+        }
+
+        $counter = 0;
+  
+        for($j=0; $j<count($nums); $j++){
+            while(!isset($colorsHash[$counter]) || !$colorsHash[$counter]){
+                $counter++;
+            }
+                      
+            if($colorsHash[$counter]){
+                $nums[$j] = $counter;
+            }else{
+                $counter++;
+                $nums[$j] = $counter;
+            }
+
+            $colorsHash[$counter]--;
+        }
+    }
+}
