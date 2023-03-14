69a42e4f0671891fc860f1e0cc7a06c69cd629a6
mohammed <m1aladham@gmail.com>
2023-03-14 17:07:06 +0000

    Sync LeetCode submission - Minimum Distance Between BST Nodes (php)

---
+++ my-folder/problems/minimum_distance_between_bst_nodes/solution.php
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function minDiffInBST($root) {
+        $prev = null;
+        $min = PHP_INT_MAX;
+        $this->DFS($root, $prev, $min);
+        
+        return $min;
+    }
+
+
+    function DFS($root, &$prev, &$min){
+        if(!$root){
+            return;
+        }
+
+        $this->DFS($root->left, $prev, $min);
+
+        if(!is_null($prev)){
+            $min = min($min, $root->val - $prev);
+        }
+
+        $prev = $root->val;
+
+        $this->DFS($root->right, $prev, $min);
+    }
+}
