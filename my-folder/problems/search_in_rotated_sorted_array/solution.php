37581f5d013faf340958d55faf26faecc85006c0
mohammed <m1aladham@gmail.com>
2023-03-15 11:32:56 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (php)

--- my-folder/problems/search_in_rotated_sorted_array/solution.php
+++ my-folder/problems/search_in_rotated_sorted_array/solution.php
@@ -9,28 +9,32 @@ class Solution {
         $l = 0;
         $r = count($nums) - 1;
 
-        while($l<=$r){
-            $m = (int) (($l+$r)/2);
+        // 4,5,6,7,0,1,2
+
+        while($l <= $r){
+            $m = floor(($r+$l)/2);
 
             if($nums[$m] === $target){
                 return $m;
             }
 
-            if($nums[$m]>=$nums[$l]){
-                if($target <= $nums[$m] && $target >= $nums[$l]){
-                    $r = $m -1;
+            if($nums[$m] < $nums[$r]){ // normal sort
+                if($nums[$m] < $target && $target <= $nums[$r]){
+                    $l = $m + 1;
                 }else{
-                    $l = $m +1;
+                    $r = $m - 1;
                 }
             }else{
-                if($target >= $nums[$m] && $target <= $nums[$r]){
-                    $l = $m +1;
+                if($target < $nums[$m] && $target >= $nums[$l]){
+                    $r = $m - 1;
                 }else{
-                    $r = $m -1;
+                    $l = $m + 1;
                 }
             }
-            
+
         }
+    
+
         return -1;
     }
 }
