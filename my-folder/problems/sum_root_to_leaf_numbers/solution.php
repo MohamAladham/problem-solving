adc7d65d31e2fb30157d8eee156a7fb5271c24f1
mohammed <m1aladham@gmail.com>
2023-04-02 23:25:03 +0000

    Sync LeetCode submission - Sum Root to Leaf Numbers (php)

---
+++ my-folder/problems/sum_root_to_leaf_numbers/solution.php
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function sumNumbers($root) {
+        $res = 0;
+        $this->DFS($root, $res, "");
+
+        return $res;
+    }
+
+    function DFS(?TreeNode $root, int &$res, string $path){
+        if(!$root){
+            return;
+        }
+
+        $path .= $root->val;
+
+        if(!$root->left && !$root->right){
+            $res += (int) $path;
+            return;
+        }
+
+        $this->DFS($root->left, $res, $path);
+        $this->DFS($root->right, $res, $path);
+    }
+}
