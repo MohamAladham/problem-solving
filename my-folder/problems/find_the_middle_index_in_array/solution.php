fc602843841e14ca2f984443989763b66c2c08cf
mohammed <m1aladham@gmail.com>
2023-04-03 21:34:44 +0000

    Sync LeetCode submission - Find the Middle Index in Array (php)

--- my-folder/problems/find_the_middle_index_in_array/solution.php
+++ my-folder/problems/find_the_middle_index_in_array/solution.php
@@ -5,39 +5,39 @@ class Solution {
      * @return Integer
      */
     function findMiddleIndex($nums) {
+        // [2, 3, -1, 8, 4]
+        // [2, 5, 4, 12,16]
+        // [16,14,11,12, 4]
 
-        $count  = count( $nums );
-        $prefxs = [];
-        $index  = -1;
+        // [1, -1, 4]
+        // [1, 0, 4]
+        // [4, 3, 4]
+        $index = -1;
+        $prefix = [];
 
-        for ( $i = 0; $i < $count; $i++ )
-        {
-            if ( $i === 0 )
-            {
-                $prefxs[] = $nums[$i];
-            } else
-            {
-                $prefxs[] = $prefxs[$i - 1] + $nums[$i];
+        for($i=0; $i<count($nums); $i++){
+            if($i===0){
+                $prefix[] = $nums[$i];
+                continue;
             }
+
+            $prefix[] = $prefix[$i-1] + $nums[$i];
         }
 
-        for ( $i = 0; $i < $count; $i++ )
-        {
-            $prefix_end = $prefxs[$count - 1] - $prefxs[$i];
+        $postfix = [];
 
-            if ( $i === 0 )
-            {
-                $prefxs[$i - 1] = 0;
+        for($i=count($nums)-1; $i>=0; $i--){
+            if($i===count($nums)){
+                $postfix[$i] = $nums[$i];
+            }else{
+                $postfix[$i] = $postfix[$i+1] + $nums[$i];
             }
+                
 
-            
-            if ( $prefxs[$i - 1] === $prefix_end )
-            {
+            if($postfix[$i] === $prefix[$i])
                 $index = $i;
-                break;
-            }
         }
-        
-        return $index;        
+
+        return $index;
     }
 }
