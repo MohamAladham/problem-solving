998dd677c80ceebd8769986a8bf47513d1594b08
mohammed <m1aladham@gmail.com>
2023-04-04 21:22:55 +0000

    Sync LeetCode submission - Max Consecutive Ones III (php)

---
+++ my-folder/problems/max_consecutive_ones_iii/solution.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer
+     */
+    function longestOnes($nums, $k) {
+        $left = 0;
+        $right = 0;
+        $counts = [0, 0];
+        $max = 0;
+
+        while($right<count($nums)){
+            $counts[$nums[$right]]++;
+            
+            if($counts[0] > $k){
+                $counts[$nums[$left]]--;
+                $left++;
+            }
+            
+            $max = max($max, $right - $left + 1);
+            $right++;
+        }
+
+        return $max;
+    }
+}
