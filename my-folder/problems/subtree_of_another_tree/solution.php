5f764592cd3f4d57191b88fd86fb3e57d0bf5ea8
mohammed <m1aladham@gmail.com>
2023-04-08 14:14:51 +0000

    Sync LeetCode submission - Subtree of Another Tree (php)

---
+++ my-folder/problems/subtree_of_another_tree/solution.php
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param TreeNode $subRoot
+     * @return Boolean
+     */
+    function isSubtree($root, $subRoot) {
+        $this->subRoot = $subRoot;
+        return $this->DFS($root);        
+    }
+
+
+    function DFS($r){
+        if($r == $this->subRoot){
+            return true;
+        }
+
+        if(!$r){
+            return false;
+        }
+
+        if($this->DFS($r->left) || $this->DFS($r->right)){
+            return true;
+        }
+
+        return false;
+    }
+
+}
