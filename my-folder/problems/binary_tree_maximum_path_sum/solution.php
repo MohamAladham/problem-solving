ff0c8ead39fafe63de438e71b294e71e96d00369
mohammed <m1aladham@gmail.com>
2023-04-08 13:18:57 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (php)

---
+++ my-folder/problems/binary_tree_maximum_path_sum/solution.php
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function maxPathSum($root) {
+        $max = PHP_INT_MIN;
+        $this->DFS($root, $max);
+        return $max;
+    }
+
+
+    function DFS($root, &$max){
+        if(!$root){
+            return 0;
+        }
+
+        $maxL = max($this->DFS($root->left, $max), 0);
+        $maxR = max($this->DFS($root->right, $max), 0);
+
+        $maxBranch = max($maxL+$root->val, $maxR+$root->val);
+        $max = max($max ,$maxL + $root->val + $maxR);
+
+        return $maxBranch;
+    }
+}
