54335439ca67d543612ed8b6394c29a8490086f2
mohammed <m1aladham@gmail.com>
2023-04-08 12:33:03 +0000

    Sync LeetCode submission - Palindromic Substrings (php)

---
+++ my-folder/problems/palindromic_substrings/solution.php
@@ -0,0 +1,33 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function countSubstrings($s) {
+        // qmaddamr
+        // qmadamr
+        $count = 0;
+
+        for($i=0; $i<strlen($s); $i++){
+            $l = $i;
+            $r = $i;
+            while($l>=0 && $r<=strlen($s) && $s[$l]===$s[$r]){
+                $count++;
+                $l--;
+                $r++;
+            }
+
+            // for event string
+            $l = $i;
+            $r = $i+1;
+            while($l>=0 && $r<=strlen($s) && $s[$l]===$s[$r]){
+                $count++;
+                $l--;
+                $r++;
+            }
+        }
+
+        return $count;
+    }
+}
