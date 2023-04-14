20ccf76a8ed43ae616922890daa6ab1d98986542
mohammed <m1aladham@gmail.com>
2023-04-14 23:43:26 +0000

    Sync LeetCode submission - Rotate Image (php)

--- my-folder/problems/rotate_image/solution.php
+++ my-folder/problems/rotate_image/solution.php
@@ -5,49 +5,13 @@ class Solution {
      * @return NULL
      */
     function rotate(&$matrix) {
-        /*
-    00, 20
-    01, 10
-    02, 00 <
-    10, 21 
-    11, 11
-    12, 01 <
-    20, 22 
-    21, 12 < 
-    22, 02 < 
+        $temp = [];
 
-    ij, (count-j-1)i
-
-
-    */
-
-        $count    = count( $matrix );
-        $hash_old = [];
-
-
-        for ( $i = 0; $i < $count; $i++ )
-        {
-            for ( $j = 0; $j < $count; $j++ )
-            {
-                $hash_old[$this->hashIndexes( $i, $j )] = $matrix[$i][$j];
-
-                $new_i = $count - $j - 1;
-
-                if ( isset( $hash_old[$this->hashIndexes( $new_i, $i )] ) )
-                {
-                    $matrix[$i][$j] = $hash_old[$this->hashIndexes( $new_i, $i )];
-                } else
-                {
-                    $matrix[$i][$j] = $matrix[$new_i][$i];
-                }
+        for($i=0; $i<count($matrix); $i++){
+            for($j=0; $j<count($matrix[0]); $j++){
+                $temp[$i.'_'.$j] = $matrix[$i][$j];
+                $matrix[$i][$j] = $temp[(count($matrix)-$j-1).'_'.$i] ?? $matrix[count($matrix)-$j-1][$i];
             }
         }
-
-    }
-
-
-    function hashIndexes( $ind1, $ind2 )
-    {
-        return $ind1 . '_' . $ind2;
     }
 }
