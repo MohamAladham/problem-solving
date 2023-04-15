f47ad2b2cec9c03352c4292081f9c81cb362c1c2
mohammed <m1aladham@gmail.com>
2023-04-15 02:55:00 +0000

    Sync LeetCode submission - Maximum Subarray (php)

---
+++ my-folder/problems/maximum_subarray/solution.php
@@ -0,0 +1,18 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+     function maxSubArray($nums) {
+        $max = PHP_INT_MIN;
+        $currSum =0;
+        foreach($nums as $num){
+            if($currSum < 0) // if we have negative sum, start over and make currSum=0
+                $currSum = 0;
+            $currSum += $num;// else keep adding
+            $max = max($max, $currSum);   
+        }
+        return $max;
+    }
+}
