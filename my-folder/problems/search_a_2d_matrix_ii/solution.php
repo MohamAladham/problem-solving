dbe5c2c97e4398e21dcb887d9ab90af29b932828
mohammed <m1aladham@gmail.com>
2023-04-15 15:06:41 +0000

    Sync LeetCode submission - Search a 2D Matrix II (php)

---
+++ my-folder/problems/search_a_2d_matrix_ii/solution.php
@@ -0,0 +1,29 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $matrix
+     * @param Integer $target
+     * @return Boolean
+     */
+    function searchMatrix($matrix, $target) {
+        
+        for($i=0; $i<count($matrix); $i++){
+            $left = 0;
+            $right = count($matrix[0]) - 1;
+
+            while($left <= $right){
+                $m = floor(($left+$right)/2);
+
+                if($matrix[$i][$m] === $target){
+                    return true;
+                }elseif($matrix[$i][$m] > $target){
+                    $right = $m - 1;
+                }else{
+                    $left = $m + 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
