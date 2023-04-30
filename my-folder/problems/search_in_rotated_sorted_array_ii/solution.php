721f3a92d4d1e6e68ee55dbdb5f662f84789d1af
mohammed <m1aladham@gmail.com>
2023-04-30 15:34:00 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (php)

--- my-folder/problems/search_in_rotated_sorted_array_ii/solution.php
+++ my-folder/problems/search_in_rotated_sorted_array_ii/solution.php
@@ -6,21 +6,30 @@ class Solution {
      * @return Boolean
      */
     function search($nums, $target) {
+        //[2,5,6,7,8,9,0,0,1,2] // 0 
+        //         ^
+
+
         $l = 0;
         $r = count($nums) - 1;
 
         while($l<=$r){
-            while($l<$r && $nums[$l] === $nums[$l+1]){
+           
+            while( $nums[$l] === $nums[$l+1]){
                 $l++;
             }
-            
-            while($l<$r && $nums[$r] === $nums[$r-1]){
+
+            while( $nums[$r] === $nums[$r-1]){
                 $r--;
             }
-            
-            $m = floor(($r+$l)/2);
 
-            if($nums[$m] === $target || $nums[$r] === $target || $nums[$l] === $target){
+            $m = floor(($l+$r)/2);
+
+            if($nums[$m] === $target || $nums[$l] === $target || $nums[$r] === $target){
+                return true;
+            }
+
+                 if($nums[$m] === $target || $nums[$r] === $target || $nums[$l] === $target){
                 return true;
             }
 
@@ -37,8 +46,8 @@ class Solution {
                     $r = $m - 1;
                 }
             }
-
         }
+
 
         return false;
     }
