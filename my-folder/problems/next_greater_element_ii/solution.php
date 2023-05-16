f93200bf8cb957a13498641e1c1967020b143e37
mohammed <m1aladham@gmail.com>
2023-05-16 16:34:59 +0000

    Sync LeetCode submission - Next Greater Element II (php)

--- my-folder/problems/next_greater_element_ii/solution.php
+++ my-folder/problems/next_greater_element_ii/solution.php
@@ -1,34 +1,30 @@
 class Solution {
-/*
-
-
-
-4
-___
-
-*/
-
 
     /**
      * @param Integer[] $nums
      * @return Integer[]
      */
     function nextGreaterElements($nums) {
+        /*
+
+
+2:1
+        */
+       
         $ans = array_fill(0,count($nums), -1);
         $stack = new SplStack();
 
         for($i=1; $i<=2; $i++){
-            foreach($nums as $index=>$val){
-                while(!$stack->isEmpty() && $stack->top()[0] < $val){
-                    $e = $stack->pop();
-                    $ans[$e[1]] = $val;
+            for($j=0; $j<count($nums); $j++){
+                while(!$stack->isEmpty() && $stack->top()[0] < $nums[$j]){
+                    list($value, $index) = $stack->pop();
+                    $ans[$index] = $nums[$j];
                 }
 
-                $stack->push([$val, $index]);
+                $stack->push([$nums[$j], $j]);
             }
-
         }
-        
+
         return $ans;
     }
 }
