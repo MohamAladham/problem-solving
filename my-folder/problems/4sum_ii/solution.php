3bd1462b98f9ea6be074be39cd2683cea60bb2db
mohammed <m1aladham@gmail.com>
2023-05-18 12:43:29 +0000

    Sync LeetCode submission - 4Sum II (php)

--- my-folder/problems/4sum_ii/solution.php
+++ my-folder/problems/4sum_ii/solution.php
@@ -9,22 +9,22 @@ class Solution {
      */
     function fourSumCount($nums1, $nums2, $nums3, $nums4) {
         $hash = [];
-        $res = 0;
+        $count = 0;
 
         foreach($nums1 as $n1){
             foreach($nums2 as $n2){
-                $hash[$n1+$n2] = isset($hash[$n1+$n2]) ? $hash[$n1+$n2]+1 : 1;
+                $hash[$n1+$n2] = isset($hash[$n1+$n2]) ? ++$hash[$n1+$n2] : 1;
             }
         }
-        
+
         foreach($nums3 as $n3){
             foreach($nums4 as $n4){
-                if(isset($hash[-1*($n3+$n4)])){
-                    $res += $hash[-1*($n3+$n4)];
+                if(isset($hash[(-1)*($n3+$n4)])){
+                    $count += $hash[(-1)*($n3+$n4)];
                 }
             }
         }
 
-        return $res;
+        return $count;
     }
 }
